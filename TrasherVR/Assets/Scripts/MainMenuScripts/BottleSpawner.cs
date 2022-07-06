fc355acead451966b5cd06de6a0bcc5f3c5842d7
Anton Nikolaev <76194166+ResicHub@users.noreply.github.com>
2022-07-06 13:48:51 +0700

    Fix bottle spawn issue
    Now a bottle spawns correctly.

--- TrasherVR/Assets/Scripts/MainMenuScripts/BottleSpawner.cs
+++ TrasherVR/Assets/Scripts/MainMenuScripts/BottleSpawner.cs
@@ -6,24 +6,38 @@ public class BottleSpawner : MonoBehaviour
 {
     [SerializeField]
     private GameObject prefab;
-    private bool isTrashOnTable;
+    private bool isTrashOnTable = true;
+    private float timer = 1f;
 
     private void OnTriggerStay(Collider other)
     {
-        isTrashOnTable = true;
+        if (!isTrashOnTable && other.tag == "Trash")
+        {
+            isTrashOnTable = true;
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Trash")
         {
-            if (!isTrashOnTable)
-            {
-                Instantiate(
+            isTrashOnTable = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        timer -= Time.deltaTime;
+        if (!isTrashOnTable)
+        {
+            Instantiate(
                 prefab,
                 gameObject.transform.position,
                 gameObject.transform.rotation);
-            }
+        }
+        if (timer <= 0)
+        {
+            timer = 1f;
         }
     }
 }
