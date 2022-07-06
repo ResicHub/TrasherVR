316483c1244658d6c5ccdd5df9dbcb0f2e1d18b0
Anton Nikolaev <76194166+ResicHub@users.noreply.github.com>
2022-07-06 14:25:10 +0700

    Adding script for credits bucket
    Now, when the player throw bottle into "CreditsBucket", a screen with information about the developer appears on the left wall of the room.

---
+++ TrasherVR/Assets/Scripts/MainMenuScripts/CreditsBucket.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsBucket : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject creditsBoard;
+    private bool isActive = false;
+    private float showingTime = 2f;
+    private Vector3 finalPosition = new Vector3(-2.9f, 1.2f, 0f);
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isActive && other.tag == "Trash" )
+        {
+            isActive = true;
+            StartCoroutine(ShowBoardCoroutine());
+        }
+    }
+
+    private IEnumerator ShowBoardCoroutine()
+    {
+        creditsBoard.SetActive(true);
+        Vector3 startPosition = creditsBoard.transform.position;
+        float showTimer = 0f;
+        while (showTimer < showingTime || creditsBoard.transform.position != finalPosition)
+        {
+            yield return creditsBoard.transform.position = Vector3.Lerp(startPosition, finalPosition, showTimer / showingTime);
+            showTimer += Time.deltaTime;
+        }
+        yield return creditsBoard.transform.position = finalPosition;
+    }
+}
