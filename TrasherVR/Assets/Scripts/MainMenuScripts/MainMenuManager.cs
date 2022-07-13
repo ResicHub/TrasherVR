58f92ba62817c03699fe453e4d9199274013d796
Anton Nikolaev <76194166+ResicHub@users.noreply.github.com>
2022-07-13 16:46:35 +0700

    Start creating tutorial
    Start making Oculus Input system.

--- TrasherVR/Assets/Scripts/MainMenuScripts/MainMenuManager.cs
+++ TrasherVR/Assets/Scripts/MainMenuScripts/MainMenuManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenuManager : MonoBehaviour
 {
     public static MainMenuManager Instance;
+
+    [SerializeField]
+    private TextMeshPro prompt;
 
     private void Awake()
     {
@@ -12,6 +16,33 @@ public class MainMenuManager : MonoBehaviour
         {
             Instance = this;
         }
+    }
+
+    private void Start()
+    {
+        prompt.text = "Press 'A' to open settings\n(lover button of right controller)";
+    }
+
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            StartSettings();
+        }
+    }
+
+    private void StartSettings()
+    {
+        prompt.text =
+            "Use secondary tumb sticks to grab the bottle.\n" +
+            "Use left stick to change distance to the table,\n" +
+            "and right stick to change height.\n" +
+            "Make sure you are comfortable grabbing the object on the table.";
     }
 
     public void StartGame()
